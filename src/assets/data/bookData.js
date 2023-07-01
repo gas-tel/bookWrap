ee3bcc94829d795d6a283ec9ee8cb99d1f6c1f09
shogun91 <shogun91@naver.com>
2023-07-01 20:02:34 +0900

    이미지 절대경로

--- src/assets/data/bookData.js
+++ src/assets/data/bookData.js
@@ -9,7 +9,7 @@ export default [
         order : false,
         event : false,
         salesValue : 125,
-        imgSrc : require("../img/books/book1.jpeg"),
+        imgSrc : require("@/assets/img/books/book1.jpeg"),
         price : 20000,
         sale : 10,
         info : `"영화를 만들어낸 토대가 이 한 권에 담겼다.
@@ -31,7 +31,7 @@ export default [
         order : false,
         event : true,
         salesValue : 45,
-        imgSrc : require("../img/books/book2.jpeg"),
+        imgSrc : require("@/assets/img/books/book2.jpeg"),
         price : 18000,
         sale : 10,
         info : `“백성이 필요로 하기 때문이다.”
@@ -48,7 +48,7 @@ export default [
         order : false,
         event : false,
         salesValue : 55,
-        imgSrc : require("../img/books/book3.jpeg"),
+        imgSrc : require("@/assets/img/books/book3.jpeg"),
         price : 18000,
         sale : 10,
         info : `100세 시대에 ‘갓생 1세대’, 40대를 재해석하다
@@ -66,7 +66,7 @@ export default [
         order : false,
         event : false,
         salesValue : 77,
-        imgSrc : require("../img/books/book4.jpeg"),
+        imgSrc : require("@/assets/img/books/book4.jpeg"),
         price : 5500,
         sale : 0,
         info : `세계는 신시대!
@@ -88,7 +88,7 @@ export default [
         order : false,
         event : true,
         salesValue : 34,
-        imgSrc : require("../img/books/book5.jpeg"),
+        imgSrc : require("@/assets/img/books/book5.jpeg"),
         price : 19000,
         sale : 10,
         info : `정해진 부의 미래, 배터리 혁명은 이제 시작이다!
@@ -106,7 +106,7 @@ export default [
         order : false,
         event : false,
         salesValue : 95,
-        imgSrc : require("../img/books/book40.jpg"),
+        imgSrc : require("@/assets/img/books/book40.jpg"),
         price : 20000,
         sale : 10,
         info : `"영화를 만들어낸 토대가 이 한 권에 담겼다.
@@ -128,7 +128,7 @@ export default [
         order : false,
         event : false,
         salesValue : 32,
-        imgSrc : require("../img/books/book6.jpeg"),
+        imgSrc : require("@/assets/img/books/book6.jpeg"),
         price : 18000,
         sale : 10,
         info : `지난 60여 년간 전 세계 문단과 독자들의 열렬한 지지를 받아온 작가, 존 르 카레의 유작 『실버뷰』가 알에이치코리아에서 출간된다. 『민감한 진실』이 나온 이래 십여 년에 걸쳐 퇴고를 반복해오다 결국 세상에 내놓지 못한 채 미완으로 남아있던 원고를, 아들이자 소설가인 닉 콘웰이 마무리해 발표한 공식적으로 존 르 카레의 스물여섯 번째 장편소설이다. 냉전 직후에 벌어져 대중은 알기조차 어려운 굵직한 정치적 사건들을 21세기 영국으로 가져와 시점과 배경을 탈바꿈해, 스파이라는 조직이 가진 정치적 양면성에 입혀 사건을 풀어가는 수작이다.` 
@@ -143,7 +143,7 @@ export default [
         order : false,
         event : false,
         salesValue : 36,
-        imgSrc : require("../img/books/book7.jpeg"),
+        imgSrc : require("@/assets/img/books/book7.jpeg"),
         price : 23000,
         sale : 10,
         info : `후반부로 접어들면서 은용은 황기석으로 인해 잃은 돈을 돌려주겠다며 명 회장을 회유했고, 박준경은 명 회장에게 황기석의 죄를 묻는 참고인 조사를 시작했다. 명 회장은 황기석이 윤혜린을 죽음으로 몰고 간 수사 조작 사건의 진범임을 증언했고, 은용은 황기석에게 해당 상황을 알리며 “지금이 마지막 기회”라는 으름장을 놨다. 장태춘 역시 이 부장에게 동부지검으로 출정 나간 명 회장을 밖으로 빼돌리라고 지시했다는 자백을 받아냈다. 법과 쩐의 카르텔로 결탁한 죄인들이 자신의 이익을 위해 서로를 파멸로 몰아넣는 ‘죄수의 딜레마’가 성립되면서, 명 회장과 황기석은 오창현 살인사건의 공범으로 긴급체포된다. 은용과 박준경은 마침내 편히 잠든 윤혜린의 묘비를 찾아 ‘블루넷 대표 은용’이 적힌 명함을 건네게 되는데…….` 
@@ -158,7 +158,7 @@ export default [
         order : false,
         event : false,
         salesValue : 42,
-        imgSrc : require("../img/books/book8.jpeg"),
+        imgSrc : require("@/assets/img/books/book8.jpeg"),
         price : 18000,
         sale : 10,
         info : `코로나19 장기화로 고통받던 소상공인들이 이제는 고금리, 고물가, 고환율로 더 큰 어려움을 겪고 있다. 폐업을 선택하는 소상공인들도 많지만 아이러니하게도 통계청 자료에 따르면, 2022년에 자영업자는 11만 9,000명 늘어났다. 이런 경제 위기 상황에서 자영업자 수는 줄어드는 것이 맞지만, 고용환경 또한 급격히 악화한 것이 자영업자가 증가한 배경으로 풀이된다. 양질의 일자리가 줄고, 구조조정으로 하루아침에 일자리를 잃게 되면, 창업해서 자영업자가 되는 것은 어쩔 수 없는 선택지가 된다.` 
@@ -173,7 +173,7 @@ export default [
         order : false,
         event : false,
         salesValue : 25,
-        imgSrc : require("../img/books/book9.jpeg"),
+        imgSrc : require("@/assets/img/books/book9.jpeg"),
         price : 19000,
         sale : 10,
         info : `인류의 미래를 바꿀 신기술 챗GPT,
@@ -192,7 +192,7 @@ export default [
         order : false,
         event : true,
         salesValue : 39,
-        imgSrc : require("../img/books/book10.jpeg"),
+        imgSrc : require("@/assets/img/books/book10.jpeg"),
         price : 18000,
         sale : 10,
         info : `무의식이 고객의 지갑을 어떻게 조종하는가?
@@ -210,7 +210,7 @@ export default [
         order : false,
         event : false,
         salesValue : 99,
-        imgSrc : require("../img/books/book11.jpeg"),
+        imgSrc : require("@/assets/img/books/book11.jpeg"),
         price : 14900,
         sale : 10,
         info : `토익 베스트셀러 1위! 초보를 위한 토익 입문서!
@@ -228,7 +228,7 @@ export default [
         order : false,
         event : true,
         salesValue : 29,
-        imgSrc : require("../img/books/book12.jpeg"),
+        imgSrc : require("@/assets/img/books/book12.jpeg"),
         price : 15800,
         sale : 10,
         info : `왕과 기린을 잃은 나라, 그 운명은?
@@ -245,7 +245,7 @@ export default [
         order : false,
         event : true,
         salesValue : 78,
-        imgSrc : require("../img/books/book13.jpeg"),
+        imgSrc : require("@/assets/img/books/book13.jpeg"),
         price : 11800,
         sale : 10,
         info : `전 세계가 사랑하는 가장 아름다운 이야기, 생텍쥐페리의 『어린 왕자』가 문학 평론가 황현산 선생의 번역으로 열린책들에서 출간되었다. 프랑스어 원문에 대한 섬세한 이해, 정확하고도 아름다운 문장력, 예리한 문학적 통찰을 고루 갖춘 번역으로 문학 번역에서 큰 입지를 굳혀 온 황현산 선생은 이 작품을 새롭게 번역하면서 생텍쥐페리의 진솔한 문체를 고스란히 살려 내기 위해 심혈을 기울였다. 원문 텍스트 선택부터 번역의 마무리 작업까지, 국내에 출간된 많은 [어린 왕자] 중에서도 특히 원전의 가치를 충실히 살린 한국어 결정판을 마련하고자 했다.` 
@@ -260,7 +260,7 @@ export default [
         order : false,
         event : true,
         salesValue : 31,
-        imgSrc : require("../img/books/book14.jpeg"),
+        imgSrc : require("@/assets/img/books/book14.jpeg"),
         price : 6000,
         sale : 10,
         info : `『체인소 맨』, 『룩 백』 작가 후지모토 타츠키. 그의 압도적인 재능과 히트작이 탄생하기까지의 초석이 담긴 기적의 작품집.` 
@@ -275,7 +275,7 @@ export default [
         order : false,
         event : false,
         salesValue : 15,
-        imgSrc : require("../img/books/book15.jpeg"),
+        imgSrc : require("@/assets/img/books/book15.jpeg"),
         price : 6000,
         sale : 10,
         info : `인기 작품인 『사사키와 미야노』의 첫 공식 앤솔러지 코믹으로, 국내에서도 이름이 알려진 참여 작가들의 화려한 라인업이 기대를 불러일으킨다. 각기 다른 해석으로 그려낸 '사사키와 미야노'를 볼 수 있는 기회이며, 팬이라면 대만족할 수 있는 작품. 『사사키와 미야노』시리즈 첫 앤솔러지 코믹이 등장! 호화 집필로 전해드리는 여느 때와 다른 남고생들의 일상(Boys Life)을 즐겨보세요!` 
@@ -290,7 +290,7 @@ export default [
         order : false,
         event : false,
         salesValue : 12,
-        imgSrc : require("../img/books/book16.jpeg"),
+        imgSrc : require("@/assets/img/books/book16.jpeg"),
         price : 12000,
         sale : 10,
         info : `소방(구급) 경채 수험생이 시험장에 반드시 갖고 가야하는필수이론을 중심으로 출제했다. 출제자의 촉으로 출제가능성 높은 문제 수록했다. 가능한 자세한 해설로 높은 학습능률 실현하자.
@@ -306,7 +306,7 @@ export default [
         order : false,
         event : true,
         salesValue : 22,
-        imgSrc : require("../img/books/book17.jpeg"),
+        imgSrc : require("@/assets/img/books/book17.jpeg"),
         price : 15800,
         sale : 10,
         info : `'십이국기' 모든 이야기의 프롤로그에 해당하는 마성의 아이는 '십이국기' 시리즈와 달리 현대 일본을 배경으로 하고 있다. 고등학교 교생인 주인공이 다카사토라는 신비한 학생을 만나면서 벌어지는 일들을 사회문제의 비판적 시각을 통해 그려 내고 있다. 작가는 마성의 아이를 집필하며 '십이국기' 세계관을 구상한 것으로 알려져 있다. 세계관은 하나지만 각 권에서 이야기가 완결되기 때문에 '십이국기'를 순서대로 읽지 않더라도 충분히 즐길 수 있다.` 
@@ -321,7 +321,7 @@ export default [
         order : false,
         event : false,
         salesValue : 19,
-        imgSrc : require("../img/books/book18.jpeg"),
+        imgSrc : require("@/assets/img/books/book18.jpeg"),
         price : 25000,
         sale : 10,
         info : `단기간에 효율적으로 전 범위를 압축할 수 있도록 만든 핵심 요약집이다. 시험에 꼭 나오는 핵심적인 이론을 깔끔하게 압축 정리하여 짧은 시간에 전체 내용을 체계적으로 습득할 수 있음은 물론, 주요 내용이 표로 도식화되어 있어 최종 요약집 및 암기 노트로 활용할 수 있다. 보조단에 수록된 최신 기출문제를 통해 공무원 시험의 출제 경향을 한눈에 파악할 수 있고, 더욱 충실하게 담아낸 독해와 문학 자료를 통해 변화하는 시험 유형에 대비할 수 있다. 또한 마지막 장에는 어휘와 한자의 핵심 자료를 수록하여 수험생 여러분들의 암기에 대한 부담이 줄어들도록 배려하였다.` 
@@ -336,7 +336,7 @@ export default [
         order : false,
         event : false,
         salesValue : 66,
-        imgSrc : require("../img/books/book20.jpeg"),
+        imgSrc : require("@/assets/img/books/book20.jpeg"),
         price : 15000,
         sale : 10,
         info : `컬링의 '컬'자도 모르는 선생님과 학생들. 그리고 선생님을 짝사랑하는 귀여운 여고생. 이들이 반강제(?)로 한데 모여 컬링부를 만들었다. 모였다하면 싸우기에 바쁜 컬링부. 이들의 최종목표는 전국체전 우승! 과연 좌충우돌 컬링부는 뜻한 바를 이룰 수 있을까?` 
@@ -351,7 +351,7 @@ export default [
         order : false,
         event : false,
         salesValue : 31,
-        imgSrc : require("../img/books/book21.jpeg"),
+        imgSrc : require("@/assets/img/books/book21.jpeg"),
         price : 15000,
         sale : 10,
         info : `《반장으로서의 책임과 의무》는 친구들과 사이좋게 지내고 부모님 말씀도 잘 듣는 고등학생들의 이야기입니다. 우등생이자 모범생인 반장 이아랑, 모범생으로 보이고 싶지 않은 우등생 곽연두, 우등생이 되고 싶은 모범생 하은이 만드는 에피소드를 통해 주인공들과 같은 시기를 보내고 있는 청소년뿐 아니라 이미 청소년 시기를 지나온 성인에게도 학창 시절의 추억을 떠올리게 만드는 작품이다. 《반장으로서의 책임과 의무》는 2011년 대한민국 창작만화 공모전에서 대상을 받은 같은 제목의 단편 만화를 확장한 만화이다.` 
@@ -366,7 +366,7 @@ export default [
         order : false,
         event : false,
         salesValue : 21,
-        imgSrc : require("../img/books/book22.jpeg"),
+        imgSrc : require("@/assets/img/books/book22.jpeg"),
         price : 15000,
         sale : 10,
         info : `『여자력女自力』으로 첫번째 단행본을 선보였던 젊은 만화가 테마단편집 시리즈의 두번째 이야기가 돌아왔다. 두번째 단행본의 주제는 ‘일탈’과 ‘땡땡이’. 가보지 않은 길로 내딛는 한 걸음, 작은 세계와 일상을 벗어나는 한 걸음들이 모여 도착한 곳은 어디일까? 쉬운 길도 헤매는 세상 속에서 늘 가던 뻔하고, 쉽고, 빠른 ‘그 길’로 가지 않은 다섯 주인공들. 경쾌하고 대담한 발걸음으로 조금은 돌아가기를 택한 그들의 유쾌하고 신비로운 여정을 따라가보자.` 
@@ -381,7 +381,7 @@ export default [
         order : false,
         event : false,
         salesValue : 71,
-        imgSrc : require("../img/books/book23.jpeg"),
+        imgSrc : require("@/assets/img/books/book23.jpeg"),
         price : 30000,
         sale : 10,
         info : `지갑 텅텅, 운발 없는 전과자에 이혼남인 스콧 랭. 앤트맨으로서 완전히 새롭고, 전혀 색다른 인생을 시작하려는 그에게 또다시 인간관계의 ‘돌개바람’이 몰아친다! 새로 시작한 사업, 미심쩍은 직원들, 영 어벤저였던 사춘기 딸 덕분에 너무나도 바쁜 스콧. 거기에 끊임없이 눈앞으로 슈퍼빌런을 배달해 오는 어플리케이션 ‘헨치’까지 등장하다니! ‘히어로로 사는 게 이렇게 힘들면 다시 범죄자의 길을 걷는 건 어떨까?’라는 생각이 드는 찰나, 스콧에게 좋은 아이디어가 떠오르는데….` 
@@ -396,7 +396,7 @@ export default [
         order : false,
         event : false,
         salesValue : 61,
-        imgSrc : require("../img/books/book24.jpeg"),
+        imgSrc : require("@/assets/img/books/book24.jpeg"),
         price : 8500,
         sale : 10,
         info : `영국왕립지리학회 최초의 여성 회원이자 『조선과 그 이웃 나라들』의 저자인 이사벨라 버드의 일본 오지 탐험기! 드디어 동북지방에 도착한 버드 일행은, 일본의 제지 기술과 장례를 체험하게 된다! 격동의 동북편 스타트!` 
@@ -411,7 +411,7 @@ export default [
         order : false,
         event : false,
         salesValue : 45,
-        imgSrc : require("../img/books/book25.jpeg"),
+        imgSrc : require("@/assets/img/books/book25.jpeg"),
         price : 15000,
         sale : 10,
         info : `다홍 작가의 손에서 피어난 따뜻한 이야기
@@ -429,7 +429,7 @@ export default [
         order : false,
         event : false,
         salesValue : 11,
-        imgSrc : require("../img/books/book26.jpeg"),
+        imgSrc : require("@/assets/img/books/book26.jpeg"),
         price : 16000,
         sale : 10,
         info : `“이 소설은 여러 편의 실화를 바탕으로 한 이야기이다.”
@@ -447,7 +447,7 @@ export default [
         order : false,
         event : false,
         salesValue : 24,
-        imgSrc : require("../img/books/book27.jpeg"),
+        imgSrc : require("@/assets/img/books/book27.jpeg"),
         price : 18800,
         sale : 10,
         info : `결국 이 작품은 그동안 『신참자』와 『기린의 날개』 등에 등장했던 경시청의 젊은 엘리트 형사 마쓰미야 형사가 주인공으로 등장함으로써, 일본에서 시리즈 총 판매 부수 1,200만 부를 기록한 ‘가가 형사 시리즈’의 번외편인가, 아니면 새로운 시리즈의 서막인가 하는 궁금증을 또 한번 독자들에게 남겼다. 도쿄에서 일어난 살인 사건의 범인을 추적하는 과정에서 얽히고설킨 두 가족의 불행한 과거사가 조금씩 드러나고, 상상을 초월하는 두 가족의 악연과 복잡한 운명에 젊은 형사가 고뇌한다. 진정한 가족의 의미를 되돌아보게 하는 문제작.` 
@@ -462,7 +462,7 @@ export default [
         order : false,
         event : false,
         salesValue : 41,
-        imgSrc : require("../img/books/book28.jpeg"),
+        imgSrc : require("@/assets/img/books/book28.jpeg"),
         price : 15000,
         sale : 10,
         info : `『트로피컬 나이트』는 누군가에게 힘이 되는 이야기다. 수록작 〈고기와 석류〉를 예로 들면, 이렇다. 남편이 죽고 아들도 떠나 혼자 남은 노인이 있다. 노인은 어린아이의 얼굴을 한 괴물을 우연히 만나고, 괴물을 집 안에 들이고야 만다. 노인은 괴물에게 잡아먹히게 될까? 아니다. 조예은의 이야기는 전혀 다르게 흘러간다. 그의 소설은 힘든 삶을 힘들다고 말하는 데서 끝나지 않는다. 공포를 보여주지만 공포가 우리의 삶을 갉아먹도록 내버려두지 않는다. 어쨌든 삶은 계속되니까. 소설이 끝난 뒤에도 이야기는 이어지니까. 이야기가 계속되는 한 조예은의 인물들은 끝까지 살아내고 버틴다. 삶이 계속되는 한 조예은의 이야기는 반드시 밝은 방향으로 나아간다. 신작 소설 『트로피컬 나이트』 또한 그렇다.` 
@@ -477,7 +477,7 @@ export default [
         order : false,
         event : false,
         salesValue : 1,
-        imgSrc : require("../img/books/book29.jpeg"),
+        imgSrc : require("@/assets/img/books/book29.jpeg"),
         price : 16800,
         sale : 10,
         info : `“내 나이 일흔, 생애 처음이자 마지막이 될 집을 지었다”
@@ -495,7 +495,7 @@ export default [
         order : false,
         event : false,
         salesValue : 9,
-        imgSrc : require("../img/books/book30.jpeg"),
+        imgSrc : require("@/assets/img/books/book30.jpeg"),
         price : 12000,
         sale : 10,
         info : `“당신의 동네를 좋아하고 있습니까?”
@@ -513,7 +513,7 @@ export default [
         order : false,
         event : false,
         salesValue : 45,
-        imgSrc : require("../img/books/book31.jpeg"),
+        imgSrc : require("@/assets/img/books/book31.jpeg"),
         price : 24000,
         sale : 0,
         info : `“메모같이 사소한 일에 무슨 의미가 있을까?” 이런 질문에 CBS 라디오 PD 정혜윤은 되묻는다. 우리는 항상 사소한 것들의 도움 및 방해를 받고 있지 않냐고. 강아지가 꼬리만 흔들어도 웃을 수 있지 않냐고, 미세먼지만 심해도 우울하지 않냐고, 소음만 심해도 떠나고 싶지 않냐고. 그리고 덧붙인다. 몇 문장을 옮겨 적고 큰 소리로 외우는 것은 전혀 사소한 일이 아니라고. ‘사소한 일’이란 말을 언젠가는 ‘자그마한 기적’이라고 부르고 싶어질 것이라고. 『아무튼, 메모』는 메모는 삶을 위한 재료이자 예열 과정이라고 믿는 한 메모주의자의 기록으로, 비메모주의자가 메모주의자가 되고, 꿈이 현실로 부화하고, 쓴 대로 살 게 된 이야기이다. 그리고 무엇보다 메모장 안에서 더 용감해진 이야기이다.` 
@@ -528,7 +528,7 @@ export default [
         order : false,
         event : false,
         salesValue : 6,
-        imgSrc : require("../img/books/book32.jpeg"),
+        imgSrc : require("@/assets/img/books/book32.jpeg"),
         price : 22000,
         sale : 5,
         info : `이 책은 김명순의 시로부터 「의심의 소녀」, 「칠면조」, 「돌아다볼 때」, 「외로운 사람들」, 「탄실이와 주영이」, 「꿈 묻는 날 밤」, 「나는 사랑한다」, 「손님」, 「모르는 사람같이」 등 가능한 한 많은 작품들을 분석함으로써 김명순이라는 작가의 전모를 파악하고자 하였다. 그리고 요즘의 사회적 관심사가 된 여성 혐오나 분노 감정 같은 문제도 김명순의 작품을 통해 접근함으로써 현대에도 김명순이 새롭게 읽힐 수 있는 작가라는 점을 인식시키고자 하였다.` 
@@ -543,7 +543,7 @@ export default [
         order : false,
         event : false,
         salesValue : 11,
-        imgSrc : require("../img/books/book33.jpeg"),
+        imgSrc : require("@/assets/img/books/book33.jpeg"),
         price : 16800,
         sale : 10,
         info : `“평화롭고 웃음 넘치는 멋진 한 해가 되기를!”
@@ -560,7 +560,7 @@ export default [
         order : false,
         event : false,
         salesValue : 16,
-        imgSrc : require("../img/books/book34.jpeg"),
+        imgSrc : require("@/assets/img/books/book34.jpeg"),
         price : 17000,
         sale : 10,
         info : `
@@ -580,7 +580,7 @@ export default [
         order : false,
         event : false,
         salesValue : 40,
-        imgSrc : require("../img/books/book35.jpeg"),
+        imgSrc : require("@/assets/img/books/book35.jpeg"),
         price : 16000,
         sale : 10,
         info : `이 책은 저자가 낯선 땅 미국에 와서 겪었던 시행착오와 스탠퍼드에서 몸소 느낀 것들을 담았다. 자신에게 집중할수록 생기는 변화의 과정과 함께 그동안 깨닫고 느낀 것들, 학생으로서 공부법을 찾고 실행해온 다양한 노하우를 소개하고 있으니 이 책이 미래를 준비하는 많은 사람에게 도움이 되길 바란다. 저자는 대학 입시를 앞두거나 미국 유학을 준비하고 있는 사람들에게 도움이 될 수 있었으면 하는 마음을 가지고 이 책을 집필하였다. ‘모든 답은 외부에서 찾지 말고, 스스로 돌아보라’는 저자의 메시지를 통해 진정한 공부를 찾아가는 사람들에게 동기부여가 되기를 희망한다. ` 
@@ -595,7 +595,7 @@ export default [
         order : false,
         event : false,
         salesValue : 55,
-        imgSrc : require("../img/books/book36.jpeg"),
+        imgSrc : require("@/assets/img/books/book36.jpeg"),
         price : 18800,
         sale : 10,
         info : `문과생도 중고등학생도 직장인도 프로그래밍에 눈뜨게 만든 바로 그 책이 전면 개정판으로 새로 태어났다! 2016년 《Do it! 점프 투 파이썬》으로 출간되었던 이 책은 약 4년 동안의 피드백을 반영하여 초보자가 더 빠르게 입문하고, 더 깊이 있게 공부할 수 있도록 개정되었다. 특히 ‘나 혼자 코딩’과 ‘코딩 면허 시험 20제’ 등 독자의 학습 흐름에 맞게 문제를 보강한 점이 눈에 띈다. 실습량도 두 배로 늘었다.` 
@@ -610,7 +610,7 @@ export default [
         order : false,
         event : false,
         salesValue : 101,
-        imgSrc : require("../img/books/book37.jpeg"),
+        imgSrc : require("@/assets/img/books/book37.jpeg"),
         price : 18800,
         sale : 10,
         info : `은밀하게 금기를 무너뜨리는 치명적 손길, 그늘 속 사랑
@@ -629,7 +629,7 @@ export default [
         order : false,
         event : false,
         salesValue : 31,
-        imgSrc : require("../img/books/book38.jpeg"),
+        imgSrc : require("@/assets/img/books/book38.jpeg"),
         price : 13700,
         sale : 0,
         info : `이 교재는 통계학의 기본 개념을 수학적 지식을 깊이 요하지 않는 범위에서 개념적으로 소개한 책이다. 이 과목은 통계학 및 데이터과학의 기본 개념을 전반적으로 소개하여 통계·데이터과학을 전공하기 시작하는 학생이나, 통계분석이 필요한 사회과학이나 자연과학 분야의 학생에게도 유용할 것이다. 아울러 교재에서 다룬 통계 소프트웨어인 R을 활용하여 소개한 예제와 연습문제를 직접 해결하면 통계적 개념과 분석기법을 이해하는 데 도움이 될 것이다. 교재의 내용을 학습하다가 보다 깊은 이론적 접근이나 다양한 응용이 필요하다면 관련 교과목을 학습하면 된다.` 
@@ -644,7 +644,7 @@ export default [
         order : false,
         event : false,
         salesValue : 21,
-        imgSrc : require("../img/books/book39.jpeg"),
+        imgSrc : require("@/assets/img/books/book39.jpeg"),
         price : 14000,
         sale : 0,
         info : `그 어느 문자보다도 소리와의 관련성이 긴밀한 음소문자가 바로 한국어를 적는 한글이다. 이러한 장점을 십분 살리는 한국어 발음 교육을 하기 위해서는 교사가 그 원리를 꿰뚫고 있어야 한다. 그러한 이론적 토대 위에서 한국어 학습자에게 체계적인 발음 교육을 할 수 있다. 이러한 의도에 의해, 이 책은 음운론 기초 이론 부분과 발음 교육론 부분을 나누어 집필했다. 또한 실습 부분을 따로 기술함으로써 실제로 수업 현장에서 바로 적용할 수 있도록 도움을 주고자 했다.
@@ -660,7 +660,7 @@ export default [
         order : false,
         event : true,
         salesValue : 15,
-        imgSrc : require("../img/books/book41.jpg"),
+        imgSrc : require("@/assets/img/books/book41.jpg"),
         price : 18000,
         sale : 10,
         info : `소설가 장강명이 말하는 글 쓰는 일의 기쁨과 슬픔에 대하여
